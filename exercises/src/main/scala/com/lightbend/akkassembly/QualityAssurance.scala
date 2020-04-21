ec3fb14fe929d77ae1bdfbb705b2e55d69a9b138
Rob Kler <robklerjr@poli.ufrj.br>
2020-04-21 19:23:44 -0300

    Fault Tolerance
    Signed-off-by: Rob Kler <robklerjr@poli.ufrj.br>

--- exercises/src/main/scala/com/lightbend/akkassembly/QualityAssurance.scala
+++ exercises/src/main/scala/com/lightbend/akkassembly/QualityAssurance.scala
@@ -2,12 +2,22 @@ package com.lightbend.akkassembly
 
 import akka.NotUsed
 import akka.stream.scaladsl.Flow
+import akka.stream.{ActorAttributes, Supervision}
+
+object QualityAssurance{
+  case class CarFailedInspection(car: UnfinishedCar) extends IllegalStateException(s"Car failed inspection: $car")
+}
 
 class QualityAssurance {
-  val inspect: Flow[UnfinishedCar, Car, NotUsed] = {
-    Flow[UnfinishedCar].collect {
-      case UnfinishedCar(Some(color), Some(engine), wheels, upgrade) if wheels.size == 4 =>
-        Car(SerialNumber(), color, engine, wheels, upgrade)
-    }
+  import QualityAssurance._
+  val decider: Supervision.Decider = {
+    case CarFailedInspection(car) => Supervision.Resume
   }
+
+  val inspect: Flow[UnfinishedCar, Car, NotUsed] = Flow[UnfinishedCar].map {
+    case UnfinishedCar(Some(color), Some(engine), wheels, upgrade) if wheels.size == 4 =>
+      Car(SerialNumber(), color, engine, wheels, upgrade)
+    case car => throw CarFailedInspection(car)
+  }.withAttributes(ActorAttributes.supervisionStrategy(decider))
+
 }
